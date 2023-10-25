bd910516c7fc596c561125390b7605005f883881
Proxy Empire <145088867+ProxyEmpireIsTaken@users.noreply.github.com>
2023-10-25 17:42:10 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import http from "node:http";
+import createBareServer from "@tomphttp/bare-server-node";
+import path from "node:path";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const __dirname = process.cwd();
+const server = http.createServer();
+const app = express(server);
+const bareServer = createBareServer("/bare/");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+server.on("request", (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+server.on("listening", () => {
+  console.log(`Empire Unblocker listening on port 8080 ${process.env.PORT}`);
+});
+
+server.listen({
+  port: process.env.PORT,
+});
